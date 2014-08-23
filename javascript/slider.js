0e0b10db7ea06ad7d91c018721bd70e21947a4ed
juliaguar <spalia@web.de>
2014-08-24 01:46:45 +0200

    implement carousel for slider buttons

--- javascript/slider.js
+++ javascript/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   var sliderState = 0
 
-  slide = function() {
+  slideRight = function() {
     var sliderModulo = sliderState % 3;
 
     if (sliderModulo === 0) {
@@ -16,6 +16,26 @@ $(document).ready(function () {
     else {
         $('#proCon').hide()
         $('#shouldI').show()
+    }
+
+    sliderState++
+  }
+
+  slideLeft = function() {
+    var sliderModulo = sliderState % 3;
+
+    if (sliderModulo === 0) {
+        $('#shouldI').hide()
+        $('#proCon').show()
+    }
+    else if (sliderModulo === 1) {
+      $('#proCon').hide()
+      $('#optionInputForm').show()
+
+    }
+    else {
+      $('#optionInputForm').hide()
+      $('#shouldI').show()
     }
 
     sliderState++
